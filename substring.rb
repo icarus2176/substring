c2c1c6fd79dfdebdd6977ed53df4479d8d0096ae
icarus2176 <ashe.kemuri@gmail.com>
2023-03-03 13:42:34 -0600

    added code to substrings method

--- substring.rb
+++ substring.rb
@@ -1,1 +1,12 @@
-def substring(text, dictionary)
+def substrings(text, dictionary)
+  dictionary.reduce(Hash.new(0)) do |word, times_appeared|
+    changed_text = text.downcase
+    until changed_text.include?(word.downcase) == false
+      times_appeared++
+      changed_text = changed_text[changed_text.index(word) + word.length]
+    end
+  end
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
