0428b39b93d0effd82282c00910948ea010003af
icarus2176 <ashe.kemuri@gmail.com>
2023-03-03 14:32:26 -0600

    make code more streamlined

--- substring.rb
+++ substring.rb
@@ -1,16 +1,15 @@
 def substrings(text, dictionary)
   result = Hash.new(0)
+  down_text = text.downcase
 
   dictionary.each do |word|
-    down_text = text.downcase
-
-    while down_text.include?(word)
-      result[word] += 1
-      down_text.sub!(word, "")
+    match = down_text.scan(word).length
+    if match > 0
+      result[word] = match
     end
+    end
+    result
   end
-  result
-end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
